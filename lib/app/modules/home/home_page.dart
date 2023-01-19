d73da784d99e715b3b81fbc979b372b08a2ec0a3
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2023-01-19 15:15:10 -0300

    refactor: :recycle: configure list of categories

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,14 +1,48 @@
+import 'package:domining_layout/app/core/shared/app_images.dart';
+import 'package:domining_layout/app/modules/home/enitity/categories_entity.dart';
+import 'package:domining_layout/app/modules/home/widgets/list_cotegories.dart';
+import 'package:domining_layout/app/modules/home/widgets/search_box.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const SizedBox(height: 60),
+              SearchBox(
+                onChanged: (String value) {},
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Text(
+                'Categories',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const SizedBox(
+                height: 90,
+                child: CategoriesListWidgets(),
+              )
+            ],
+          ),
         ),
       ),
     );
