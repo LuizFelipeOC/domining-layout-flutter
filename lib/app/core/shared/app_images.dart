3bda7d3b7f70c080a1517be860290fa77bb620ef
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2023-01-19 16:27:02 -0300

    refactor: :recycle: add paths of products images

--- lib/app/core/shared/app_images.dart
+++ lib/app/core/shared/app_images.dart
@@ -1,4 +1,14 @@
 class AppImages {
   static String get facebookIcon => 'assets/facebook_icon.png';
   static String get googleIcon => 'assets/google_icon.png';
+  static String get iconDevice => 'assets/devices.png';
+  static String get iconGadgets => 'assets/gadgets.png';
+  static String get iconGaming => 'assets/gaming.png';
+  static String get iconMshoes => 'assets/mshoes.png';
+  static String get iconWshoes => 'assets/wshoes.png';
+
+  static String get produto => 'assets/produto1.png';
+  static String get produto2 => 'assets/produto2.png';
+  static String get produto3 => 'assets/produto3.png';
+  static String get produto4 => 'assets/produto4.png';
 }
